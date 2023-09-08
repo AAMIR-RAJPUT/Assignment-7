cfc6a7c10b8f4ce21009879a88f4f4b505cc9d57
AAMIR-RAJPUT <aamirrajput421@gmail.com>
2023-09-08 22:21:05 +0500

    work complete

---
+++ scripts.js
@@ -0,0 +1,222 @@
+//?q1
+        // Prompt the user for two numbers
+      //  var num1 = parseFloat(prompt("Enter the first number:"));
+        //var num2 = parseFloat(prompt("Enter the second number:"));
+
+        // Add the two numbers and store the result in a variable
+        //var sum = num1 + num2;
+
+        // Display the result in the browser
+        //document.write("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+//?q2
+         // Prompt the user for two numbers
+        // var num1 = parseFloat(prompt("Enter the first number:"));
+         //var num2 = parseFloat(prompt("Enter the second number:"));
+ 
+         // Perform subtraction
+         //var subtraction = num1 - num2;
+ 
+         // Perform multiplication
+         //var multiplication = num1 * num2;
+ 
+         // Perform division
+        // var division = num1 / num2;
+ 
+         // Perform modulus
+         //var modulus = num1 % num2;
+ 
+         // Display the results in the browser
+         //document.write("Subtraction: " + num1 + " - " + num2 + " = " + subtraction + "<br>");
+         //document.write("Multiplication: " + num1 + " * " + num2 + " = " + multiplication + "<br>");
+         //document.write("Division: " + num1 + " / " + num2 + " = " + division + "<br>");
+         //document.write("Modulus: " + num1 + " % " + num2 + " = " + modulus + "<br>");
+
+//?q3
+
+           // Declare a variable
+     //   var myVariable;
+
+        // Show the value of the variable after declaration
+       // document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+        // Initialize the variable with some number
+        //myVariable = 5;
+
+        // Show the initial value of the variable
+        //document.write("Initial value: " + myVariable + "<br>");
+
+        // Increment the variable
+        //myVariable++;
+
+        // Show the value of the variable after increment
+        //document.write("Value after increment is: " + myVariable + "<br>");
+
+        // Add 7 to the variable
+        //myVariable += 7;
+
+        // Show the value of the variable after adding 7
+        //document.write("Value after adding 7 is: " + myVariable + "<br>");
+        
+//?q4
+
+         // Declare a variable
+     //    var myVariable;
+
+         // Show the value of the variable after declaration
+       //  document.write("Value after variable declaration is: " + myVariable + "<br>");
+ 
+         // Initialize the variable with some number
+         //myVariable = 5;
+ 
+         // Show the initial value of the variable
+         //document.write("Initial value: " + myVariable + "<br>");
+ 
+         // Increment the variable
+         //myVariable++;
+ 
+         // Show the value of the variable after increment
+         //document.write("Value after increment is: " + myVariable + "<br>");
+ 
+         // Add 7 to the variable
+         //myVariable += 7;
+ 
+         // Show the value of the variable after adding 7
+//         document.write("Value after addition is: " + myVariable + "<br>");
+ 
+         // Decrement the variable
+  //       myVariable--;
+ 
+         // Show the value of the variable after decrement
+    //     document.write("Value after decrement is: " + myVariable + "<br>");
+ 
+         // Calculate and show the remainder after dividing by 3
+      //   var remainder = myVariable % 3;
+        // document.write("The remainder is: " + remainder + "<br>");
+ //?q5
+
+ // Store the ticket price in a variable
+//var ticketPrice = 600; // 600 PKR per ticket
+
+// Calculate the cost of buying 5 tickets
+//var numberOfTickets = 5;
+//var totalCost = ticketPrice * numberOfTickets;
+
+// Display the total cost
+//console.log("The cost of buying 5 tickets is: " + totalCost + " PKR");
+//?q6
+        // Prompt the user for a number
+//        var number = parseInt(prompt("Enter a number for the multiplication table:"));
+
+        // Display the multiplication table in the browser
+  //      document.write("<h2>Multiplication Table for " + number + "</h2>");
+    //    for (var i = 1; i <= 10; i++) {
+      //      var result = number * i;
+        //    document.write(number + " x " + i + " = " + result + "<br>");
+       // }
+//?q7
+// Part A: Store a Celsius temperature into a variable
+//var celsiusTemperature = 25; // Replace with your desired Celsius temperature
+
+// Part B: Convert Celsius to Fahrenheit
+//var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+
+// Output the result
+//console.log(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F");
+
+// Part C: Store a Fahrenheit temperature into a variable
+//var fahrenheitTemperature2 = 77; // Replace with your desired Fahrenheit temperature
+
+// Part D: Convert Fahrenheit to Celsius
+//var celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5/9;
+
+// Output the result
+//console.log(fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C");
+
+//?q8
+
+  // Total marks and marks obtained by the student
+//  var totalMarks = 1000; // Replace with the total marks for the exam
+  //var marksObtained = 850; // Replace with the marks obtained by the student
+
+  // Calculate the percentage
+  //var percentage = (marksObtained / totalMarks) * 100;
+
+  // Display the result in the browser
+  //document.write("<h2>Percentage Calculator</h2>");
+  //document.write("<p>Total Marks: " + totalMarks + "</p>");
+  //document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+  //document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
+//?q9
+//var usDollars = 10;
+//var saudiRiyals = 25;
+
+// Exchange rates
+//var usdToPkrRate = 104.80;
+//var sarToPkrRate = 28;
+
+// Calculate the total amount in Pakistani Rupees
+//var totalPkr = (usDollars * usdToPkrRate) + (saudiRiyals * sarToPkrRate);
+
+// Display the total amount in Pakistani Rupees
+//console.log("Total amount in Pakistani Rupees: " + totalPkr + " PKR");
+//?q10
+// Initialize the variable with some number
+//var initialNumber = 10; // Replace with your desired initial number
+
+// Perform the arithmetic operations in a single expression
+//var result = ((initialNumber + 5) * 10) / 2;
+
+// Display the result
+//console.log("Result after arithmetic operations: " + result);
+
+//?q11
+// Store the current year in a variable
+//var currentYear = new Date().getFullYear();
+
+// Store their birth year in a variable (replace with the actual birth year)
+//var birthYear = 1990;
+
+// Calculate their two possible ages based on the stored values
+//var age1 = currentYear - birthYear;
+//var age2 = age1 - 1; // In case the birthday hasn't occurred yet this year
+
+// Output the ages to the screen
+//console.log("They are either " + age1 + " or " + age2 + " years old.");
+
+//?q12
+// Define the radius of the circle (replace with the actual radius)
+//var radius = 5;
+
+// Calculate the circumference using the formula: 2 * π * radius
+//var circumference = 2 * 3.142 * radius;
+
+// Calculate the area using the formula: π * radius^2
+//var area = 3.142 * radius * radius;
+
+// Output the circumference and area
+//console.log("The circumference is " + circumference.toFixed(2));
+//console.log("The area is " + area.toFixed(2));
+
+//?q13
+
+
+// Store your favorite snack into a variable
+//var favoriteSnack = "chocolate bars"; // Replace with your favorite snack
+
+// Store your current age into a variable
+//var currentAge = 30; // Replace with your current age
+
+// Store a maximum age into a variable
+//var maximumAge = 80; // Replace with your maximum desired age
+
+// Store an estimated amount per day (as a number)
+//var snacksPerDay = 2; // Replace with the estimated amount per day
+
+// Calculate how many snacks you would need for the rest of your life
+//var yearsRemaining = maximumAge - currentAge;
+//var totalSnacksNeeded = yearsRemaining * 365 * snacksPerDay;
+
+// Output the result to the screen
+//console.log("You will need " + totalSnacksNeeded + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge + ".");
